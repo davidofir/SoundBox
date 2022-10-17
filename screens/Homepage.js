c3624ea653c808853af4c111cffa1ef841f20801
miguelledelapaz <miguelledelapaz@yahoo.com>
2022-10-17 00:03:17 -0400

    Added login

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -2,22 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-export default Homepage = ({navigation}) =>{
+export default Homepage = ({ navigation }) => {
     return (
-    <View style={styles.container}>
-        <TouchableOpacity style={{marginRight:8,padding:10}} onPress={()=>navigation.navigate("Discover")}>
-            <Text>Discover Artists</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{marginRight:8,padding:10}} onPress={()=>navigation.navigate("Artist")}>
-            <Text>Artist Profile</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{marginRight:8,padding:10}} onPress={()=>navigation.navigate("Upcoming Events")}>
-            <Text>Upcoming Events</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{marginRight:8,padding:10}} onPress={()=>navigation.navigate("Merch Store")}>
-            <Text>Store</Text>
-        </TouchableOpacity>
-    </View>)
+        <View style={styles.container}>
+            <TouchableOpacity style={{ marginRight: 8, padding: 10 }} onPress={() => navigation.navigate("Discover")}>
+                <Text>Discover Artists</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginRight: 8, padding: 10 }} onPress={() => navigation.navigate("Artist")}>
+                <Text>Artist Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginRight: 8, padding: 10 }} onPress={() => navigation.navigate("Upcoming Events")}>
+                <Text>Upcoming Events</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginRight: 8, padding: 10 }} onPress={() => navigation.navigate("Merch Store")}>
+                <Text>Store</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginRight: 8, padding: 10 }} onPress={() => navigation.navigate("Merch Store")}>
+                <Text>Login</Text>
+            </TouchableOpacity>
+        </View>)
 }
 const styles = StyleSheet.create({
     container: {
