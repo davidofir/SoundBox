68a8774240f2d195db66ece559d95436f18e9e30
miguelledelapaz <miguelledelapaz@yahoo.com>
2022-10-17 00:40:00 -0400

    added imports

---
+++ screens/Login.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
