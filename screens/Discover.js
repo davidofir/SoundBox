8c21c21366e0c2e40ea37203197707aa6cde9543
alexjones <alexjones202@outlook.com>
2022-10-16 23:24:09 -0400

    Alex's First Commit

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,19 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-export default Discover = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>Discover</Text>
+// SearchBar.js
+import React from "react";
+import { StyleSheet, TextInput, View, Keyboard, Button, SafeAreaView, Text, Alert } from "react-native";
+import { Feather, Entypo } from "@expo/vector-icons";
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+
+const SearchBar = ({clicked, searchPhrase, setSearchPhrase, setClicked}) => {
+  return (
+    <View style={styles.container}>
+      <View
+        style={
+          clicked
+            ? styles.searchBar__clicked
+            : styles.searchBar__unclicked
+        }
+      >
+        {/* search Icon */}
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
+        {/* Input field */}
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          value={searchPhrase}
+          onChangeText={setSearchPhrase}
+     
+        />
+        {/* cross Icon, depending on whether the search bar is clicked or not */}
+        {clicked && (
+          <Entypo name="cross" size={20} color="black" style={{ padding: 1 }} onPress={() => {
+              setSearchPhrase("")
+          }}/>
+        )}
+      </View>
+      {/* cancel button, depending on whether the search bar is clicked or not */}
+      {clicked && (
+        <View>
+          <Button
+            title="Cancel"
+            onPress={() => {
+              Keyboard.dismiss();
+              setClicked(false);
+            }}
+          ></Button>
         </View>
-    )
-}
+      )}
+    </View>
+  );
+};
+
+
+
+
+// styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: "center",
-    },
-})
+  container: {
+    margin: 15,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "90%",
+
+  },
+  searchBar__unclicked: {
+    padding: 10,
+    flexDirection: "row",
+    width: "95%",
+    backgroundColor: "#d9dbda",
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  searchBar__clicked: {
+    padding: 10,
+    flexDirection: "row",
+    width: "80%",
+    backgroundColor: "#d9dbda",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  input: {
+    fontSize: 20,
+    marginLeft: 10,
+    width: "90%",
+  },
+});
+
+export default SearchBar;
