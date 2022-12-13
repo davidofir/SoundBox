eb67006c7772fe0bfad9de6689ed31688207e35a
alexjones <alexjones202@outlook.com>
2022-12-13 08:23:47 -0500

    Wrote basic functions for reccomendation system

--- screens/Recommendation.js
+++ screens/Recommendation.js
@@ -24,8 +24,13 @@ const Recommendations = ({navigation, route}) => {
     const topGenres = []
     const [reviews, setReviews] = useState([]);
     var userId = authentication.currentUser.uid
-
+    var reviewCount = 0
+    var reviewArray = []
+    var currentArtist = ""
+    var artistIndex = ""
+    var currentReview = 0
     // Query Firestore database with current UID
+
     useEffect(() => {
 
         const userRef = doc(db, "users", userId);
@@ -35,12 +40,68 @@ const Recommendations = ({navigation, route}) => {
                 
                 setReviews(doc.data().reviews);
                 
+                //get length of array for number of reviews
+                reviewCount = doc.data().reviews.length
+                //get the array of reviews
+                reviewArray = doc.data().reviews
+                getTopUserArtists()
             })
     }, [])
 
-
     function getTopUserArtists(){
 
+
+        
+        //for each review
+        var meanReviewList = []
+        for (let i = 0; i < reviewCount; i++ ){
+            //get the current artist name
+            currentArtist = reviewArray[i].artistName
+            currentReview = reviewArray[i].rating
+            //load current review and put artist and rating into an array
+
+            //value = true if the current artist is already in the list | else = false
+            var isAlreadyInList = meanReviewList.some((product) => product.name.includes(currentArtist))
+
+            //check if the artist is already in the list
+            if (isAlreadyInList == true){
+                //get the index of the artist
+                artistIndex = meanReviewList.findIndex(obj => obj.name === currentArtist)
+
+              
+                //add one to the count
+                meanReviewList[artistIndex].count ++
+                //add the review score to that total artists score
+                meanReviewList[artistIndex].rating =  meanReviewList[artistIndex].rating + currentReview
+                
+
+         
+            } else {
+
+                //if the artist is not already in the list, add it
+                meanReviewList.push({name: reviewArray[i].artistName, rating: reviewArray[i].rating, count: 1})
+            }
+
+
+        }
+        //console.log(meanReviewList)
+
+        var finalListCount = meanReviewList.length
+        var totalScore = 0
+        //take the average of each of the reviews now in the list
+        for (let j = 0; j < finalListCount; j++ ){
+            //get the current artist name
+            
+            //for each of the reviews
+            //find the mean by dividing total score of reviews by the amount of reviews for that artist
+            totalScore = meanReviewList[j].rating / meanReviewList[j].count
+            
+            //put the total score back in the review array
+            meanReviewList[j].rating = totalScore
+
+            
+        }
+        console.log(meanReviewList)
     }
 
     return (
