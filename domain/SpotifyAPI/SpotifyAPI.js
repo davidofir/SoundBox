c38e88f0e2249e2912c595f44a1ce3c15576ea5e
alexjones <alexjones202@outlook.com>
2023-10-09 00:11:34 -0400

    fix for special character songsnot finding images

--- domain/SpotifyAPI/SpotifyAPI.js
+++ domain/SpotifyAPI/SpotifyAPI.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
-import base64 from 'base-64'; // Import the base-64 library
+import base64 from 'base-64';
 
-// Define your Spotify API credentials
 const clientId = '78446884c91b415489c2e419606a8f75';
 const clientSecret = '330da908bba34426ac4e05f220370032';
 
-// Create a function to obtain an access token
 async function getAccessToken() {
   try {
     const credentials = `${clientId}:${clientSecret}`;
-    const base64Credentials = base64.encode(credentials); // Encode credentials to Base64
+    const base64Credentials = base64.encode(credentials);
 
     const response = await axios.post('https://accounts.spotify.com/api/token', null, {
       params: {
@@ -31,52 +29,51 @@ async function getAccessToken() {
   }
 }
 
-  
 async function searchAndFetchSongCoverArt(songName, artistName) {
-    try {
-      // Get an access token
-      const accessToken = await getAccessToken();
-  
-      // Search for the track on Spotify
-      console.log('Searching for song:', songName, 'by artist:', artistName);
-  
-      const searchResponse = await axios.get('https://api.spotify.com/v1/search', {
-        params: {
-          q: `track:${songName} artist:${artistName}`,
-          type: 'track',
-        },
+  try {
+    const accessToken = await getAccessToken();
+    let searchQuery;
+
+    // Check if either songName or artistName contains quotes
+    if (songName.includes('"') || artistName.includes('"') || songName.includes("'") || artistName.includes("'")) {
+      // Use the modified approach for songs with quotes
+      searchQuery = encodeURIComponent(`track:${songName} artist:${artistName}`);
+    } else {
+      // Use the regular approach
+      searchQuery = `track:${songName} artist:${artistName}`;
+    }
+
+    const searchResponse = await axios.get('https://api.spotify.com/v1/search', {
+      params: {
+        q: searchQuery,
+        type: 'track',
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const track = searchResponse.data.tracks.items[0];
+
+    if (track) {
+      const trackId = track.id;
+      const albumResponse = await axios.get(`https://api.spotify.com/v1/tracks/${trackId}`, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       });
-  
-      const track = searchResponse.data.tracks.items[0];
-  
-      if (track) {
-        // Extract the track ID
-        const trackId = track.id;
-  
-        // Fetch the track's album information
-        const albumResponse = await axios.get(`https://api.spotify.com/v1/tracks/${trackId}`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-  
-        const album = albumResponse.data.album;
-  
-        // Extract the cover art URLs (e.g., album.images[0].url)
-        const coverArtUrl = album.images[0].url;
-  
-        return coverArtUrl;
-      } else {
-        throw new Error('Song not found on Spotify.');
-      }
-    } catch (error) {
-      console.error('Error fetching cover art:', error);
-      throw error;
+
+      const album = albumResponse.data.album;
+      const coverArtUrl = album.images[0].url;
+
+      return coverArtUrl;
+    } else {
+      throw new Error('Song not found on Spotify.');
     }
+  } catch (error) {
+    console.error('Error fetching cover art:', error);
+    throw error;
   }
-  
+}
 
 export { searchAndFetchSongCoverArt };
