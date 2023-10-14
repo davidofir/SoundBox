c302d0ddbcdf14de41614c1604b652f38662b091
ofir <davidof@sheridancollege.ca>
2023-10-14 01:36:43 -0400

    improved the merch page

--- screens/ArtistProfile/Store.js
+++ screens/ArtistProfile/Store.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useContext, useEffect, useState } from 'react'
-import { StyleSheet, Text, View,FlatList,Image } from 'react-native';
+import { StyleSheet, Text, View,FlatList,Image,ActivityIndicator } from 'react-native';
 import MerchItem from '../../components/MerchItem';
 import GetMerchByArtistName from '../../domain/ArtistRepository/ArtistRepository';
 import MerchRepositoryImpl from '../../domain/MerchAPI/MerchRepositoryImpl';
@@ -12,65 +12,69 @@ import useViewModel from './MerchViewModel';
 
 
     
-export default Store =({route,navigation})=>{
+export default Store = ({ route, navigation }) => {
+    const { merch, getMerchByArtistName } = useViewModel();
+    const [loading, setLoading] = useState(true);
 
-    const {merch,getMerchByArtistName} = useViewModel();
-    console.log(merch)
-    const [data,setData] = useState([]);
-    useEffect(()=>{
-        getMerchByArtistName(route.params.artistName);
-        
-    },[])
-      const renderItem = ({ item }) => (
-        <MerchItem name={item.name} price={item.price} image={item.image} url={item.url}/>
-      );
-    
-    return(
-        <View>
-            <View style={styles.verticalContainer}>
-                    <FlatList
-                        data={merch}
-                        renderItem={renderItem}
-                        keyExtractor={(item,key) => key}/>
+    useEffect(() => {
+        async function fetchData() {
+            await getMerchByArtistName(route.params.artistName);
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    const renderItem = ({ item }) => (
+        <MerchItem name={item.name} price={item.price} image={item.image} url={item.url} />
+    );
+
+    if (loading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size="large" color="#0000ff" />
             </View>
-        </View>
-    )
-}
-const Item = ({ name,image }) => (
-    <View style={styles.item}>
-        <Image style={styles.square} source={{
-            uri:image
-        }}/>
-            <Text style={{textAlignVertical:"center"}}>{name}</Text>
-    </View>
-  ); 
-const styles = StyleSheet.create({
-    verticalContainer: {
-        backgroundColor: '#fff',
-        flexDirection: "row",
-        alignItems: 'center',
-        margin: 10,
-        width: 'auto',
-        height: "auto",
+        );
+    }
 
-    },
-    horizontalContainer: {
-        flex: 2,
-        backgroundColor: '#fff',
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={merch}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.name}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
-        justifyContent: "flex-start",
-        marginLeft: 15,
-        flexDirection: "row"
+        backgroundColor: '#f8f8f8',  // A soft background color
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#f8f8f8',
+        paddingHorizontal: 10,
+    },
+    item: {
+        flexDirection: "row",
+        padding: 15,  // Increase padding for better spacing
+        borderRadius: 10,  // Smooth the edges
+        backgroundColor: 'white',  // Use a white background for items
+        marginBottom: 10,  // Spacing between items
+        shadowColor: '#aaa',  // Soft shadow
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 3,  // Elevation for Android
     },
     square: {
-        width: 150,
-        height: 150,
-        backgroundColor: "black",
-        marginRight: 10,
-        borderRadius: 15
+        width: 100,
+        height: 100,
+        borderRadius: 10,  // Consistent rounded edge
+        marginRight: 15,  // Space between image and text
     },
-    item:{
-        flexDirection:"row",
-        padding:10,
-    }
 });
