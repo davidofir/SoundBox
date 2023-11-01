1abb7f5b3daa9be2fe9c2f230e6ca355a55b14b9
miguelledelapaz <miguelledelapaz@yahoo.com>
2023-10-31 23:49:21 -0400

    added navigation bar at the bottom of social feed

--- screens/SocialFeed.js
+++ screens/SocialFeed.js
@@ -1,12 +1,11 @@
-import { StyleSheet, Text, View, FlatList, TouchableWithoutFeedback, Image } from 'react-native';
-import Colors from '../constants/colors';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, StyleSheet, ScrollView, FlatList } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react'
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
-import ButtonComponent from '../components/ButtonComponent';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { FontAwesome } from '@expo/vector-icons'; // Import the FontAwesome icons
 import EventsRepository from '../domain/EventsAPI/EventsRepositoryImpl';
 import { authentication, db } from '../firebase';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc } from 'firebase/firestore';
 
 const eventsRepo = new EventsRepository;
 export default SocialFeed = ({ navigation }) => {
@@ -32,21 +31,6 @@ export default SocialFeed = ({ navigation }) => {
             })
     }, [])
 
-    /* old change
-    useEffect(() => {
-        for (let i = 0; i < following.length; i++) {
-            const userRef2 = doc(db, "users", following[i]);
-            getDoc(userRef2)
-                .then((doc) => {
-                    for (let i = 0; i < doc.data().reviews.length; i++) {
-                        tempeviews.push(doc.data().reviews[i]);
-                        setReviews(tempeviews);
-                    }
-                })
-        }
-    }, [reviews])
-    */
-
     // new change
     useEffect(() => {
         for (let i = 0; i < following.length; i++) {
@@ -66,62 +50,31 @@ export default SocialFeed = ({ navigation }) => {
     }, [following])
 
     return (
-        <View>
-            {/* <View style={styles.backgroundContainer}>
-                <View style={styles.verticalProfileContainer}>
-                    <View style={[styles.horizontalProfileContainer, { padding: 6 }]}>
-                        <View style={styles.circle} />
-                        <Text>Email: {authentication.currentUser?.email}</Text>
-                    </View>
+        < View style={styles.container} >
+            <ScrollView>
+                <View style={styles.horizontalProfileContainer}>
+                    <Text style={[styles.text, { fontSize: 22, padding: 10, fontWeight: '500' }]}>Discover Artists</Text>
+                    <Text onPress={() => navigation.navigate('Discover')} style={[styles.text, { fontSize: 18, padding: 10 }]}>View all</Text>
                 </View>
-                <View style={styles.followContainer}>
-                    <View style={styles.statsBox}>
-                        <Text style={[styles.text, styles.subText]}>Posts</Text>
-                        <Text style={[styles.text, { fontSize: 24 }]}>14</Text>
-                    </View>
-                    <View style={[styles.statsBox, { borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1 }]}>
-                        <Text style={[styles.text, styles.subText]}>Followers</Text>
-                        <Text style={[styles.text, { fontSize: 24 }]}>124</Text>
-                    </View>
-                    <View style={styles.statsBox}>
-                        <Text style={[styles.text, styles.subText]}>Following</Text>
-                        <Text style={[styles.text, { fontSize: 24 }]}>119</Text>
-                    </View>
+                <View>
+                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                        <View>
+                            <Image source={require("../assets/artists/jayz.png")} style={styles.imageContainer} />
+                        </View>
+                        <View>
+                            <Image source={require("../assets/artists/Yonce.png")} style={styles.imageContainer} />
+                        </View>
+                        <View>
+                            <Image source={require("../assets/artists/cee.png")} style={styles.imageContainer} />
+                        </View>
+                        <View>
+                            <Image source={require("../assets/artists/swift.png")} style={styles.imageContainer} />
+                        </View>
+                    </ScrollView>
                 </View>
-            </View> */}
-            <View style={styles.horizontalProfileContainer}>
-                <Text style={[styles.text, { fontSize: 22, padding: 10, fontWeight: "500" }]}>Disover Artists</Text>
-                <Text onPress={() => navigation.navigate("Discover")} style={[styles.text, { fontSize: 18, padding: 10 }]}>View all</Text>
-            </View>
-            <View style={{ marginTop: 10 }}>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                    <View>
-                        <Image source={require("../assets/artists/jayz.png")} style={styles.imageContainer} />
-                    </View>
-                    <View>
-                        <Image source={require("../assets/artists/Yonce.png")} style={styles.imageContainer} />
-                    </View>
-                    <View>
-                        <Image source={require("../assets/artists/cee.png")} style={styles.imageContainer} />
-                    </View>
-                    <View>
-                        <Image source={require("../assets/artists/swift.png")} style={styles.imageContainer} />
-                    </View>
-                </ScrollView>
-                {/* <ScrollView vertical={true} showsVerticalScrollIndicator={false} style={{ marginTop: 20 }}>
-                    <View>
-                        <View style={styles.verticalImageContainer} />
-                    </View>
-                    <View>
-                        <View style={styles.verticalImageContainer} />
-                    </View>
-                </ScrollView> */}
-            </View>
-            <View style={styles.container2}>
-                <FlatList
-                    data={reviews}
-                    renderItem={({ item }) => (
-                        <View style={styles.postContainer}>
+                <View style={styles.container2}>
+                    {reviews.map((item, index) => (
+                        <View style={styles.postContainer} key={index}>
                             <View style={styles.postHeader}>
                                 <Image source={require("../assets/defaultPic.png")} style={styles.profileImage} />
                                 <Text style={styles.username}>{item.username}</Text>
@@ -133,75 +86,56 @@ export default SocialFeed = ({ navigation }) => {
                                 <Text style={styles.songName}>Song: {item.songName}</Text>
                             </View>
                         </View>
-                    )}
-                />
+                    ))}
+                </View>
+            </ScrollView>
+
+            {/* Social Media Navigation Bar */}
+            <View style={styles.navigationContainer}>
+                <TouchableOpacity>
+                    <FontAwesome name="home" size={24} color="black" />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <FontAwesome name="search" size={24} color="black" />
+
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <FontAwesome name="plus-square" size={24} color="black" />
+
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <FontAwesome name="heart" size={24} color="black" />
+
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <FontAwesome name="user" size={24} color="black" />
+
+                </TouchableOpacity>
             </View>
-        </View>
+        </View >
     )
 }
 
 const styles = StyleSheet.create({
-    verticalProfileContainer: {
-        flexDirection: "row",
-        alignItems: 'flex-start',
-        margin: 10,
-        width: 'auto',
-        height: "auto"
-
-    },
-    backgroundContainer: {
-        backgroundColor: "white"
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
     },
     container2: {
         alignItems: 'center',
         justifyContent: 'center',
-        height: "100%",
-        paddingTop: 5,
-    },
-    item: {
-        flexDirection: "row",
-        marginTop: 10,
-        padding: 10,
-        width: "100%",
-        backgroundColor: "#ddd",
-        borderRadius: 5,
-        alignItems: "center",
-    },
-    itemImage: {
-        width: 50,
-        height: 50,
-        marginRight: 10,
-    },
-    itemText: {
-        fontSize: 35
+        paddingTop: 15,
     },
     horizontalProfileContainer: {
-        alignItems: 'center',
-        justifyContent: "flex-start",
-        flexDirection: "row"
-    },
-    circle: {
-        width: 100,
-        height: 100,
-        backgroundColor: "black",
-        marginRight: 10,
-        borderRadius: 100,
-    },
-    followContainer: {
-        flexDirection: "row",
-        alignSelf: "center",
-        marginTop: 15
-    },
-    statsBox: {
-        alignItems: "center",
-        flex: 1,
-        paddingBottom: 15
-    },
-    subText: {
-        fontSize: 12,
-        color: "#AEB5BC",
-        textTransform: "uppercase",
-        fontWeight: "500"
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        margin: 10,
+        width: 'auto',
+        height: 'auto',
     },
     imageContainer: {
         width: 120,
@@ -209,22 +143,15 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         backgroundColor: "black",
         overflow: "hidden",
-        marginHorizontal: 6
-    },
-    verticalImageContainer: {
-        width: 370,
-        height: 250,
-        borderRadius: 12,
-        backgroundColor: "grey",
-        overflow: "hidden",
-        marginVertical: 6,
-        marginHorizontal: 10
+        marginHorizontal: 6,
     },
     postContainer: {
         backgroundColor: 'white',
         borderRadius: 10,
         margin: 10,
         padding: 10,
+        width: 370,
+        minHeight: 150,
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
@@ -237,6 +164,7 @@ const styles = StyleSheet.create({
     postHeader: {
         flexDirection: 'row',
         alignItems: 'center',
+        marginBottom: 10,
     },
     profileImage: {
         width: 40,
@@ -266,5 +194,13 @@ const styles = StyleSheet.create({
     songName: {
         fontSize: 14,
         marginTop: 5,
+    },
+    navigationContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: 'white',
+        paddingVertical: 10,
+        borderTopWidth: 1,
+        borderTopColor: '#ccc',
     },
 })
